99bc770cebe8b1adeade30316c5b6f5bf41476ba
notherposeidon <136441477+notherposeidon@users.noreply.github.com>
2024-03-13 13:30:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ require('dotenv').config()
 const wait = require('wait')
 const Bitzxier = require(`./structures/Bitzxier.js`)
 const client = new Bitzxier()
-this.config = require(`${process.cwd()}/config.json`);
 (async () => {
     await client.initializeMongoose()
     await wait(3000)
     await client.loadEvents()
     await client.loadMain()
-    await client.login(this.config.TOKEN)
+    await client.login("TOKEN")
 })()
